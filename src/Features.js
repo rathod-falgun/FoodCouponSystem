b8a997d472ce73e4611493bd55096990ad588ba4
Falgun Rathod <rathodfalgun05@gmail.com>
2026-08-25 13:48:55 +0530

    Features.js
    Updated HTML syntax to use double quotes and improved text formatting.

--- src/Features.js
+++ src/Features.js
@@ -1,43 +1,79 @@
-import React from 'react'
+import React from "react";
 import {
   FaUtensils,
   FaTicketAlt,
   FaShippingFast,
   FaShieldAlt,
 } from "react-icons/fa";
-import "./Features.css"
+import "./Features.css";
 
 export default function Features() {
   return (
-    <section className='features'>
-      <h2 className='featurehanding'>Why Choose Us?</h2>
+    <section className="features">
 
-      <div className='featurecontainer'>
-        <div className='featurecard'>
-          <div className='icon'><FaUtensils /></div>
+      <h2 className="featureheading">
+        Why Choose Us?
+      </h2>
+
+      <div className="featurecontainer">
+
+        <div className="featurecard">
+          <div className="icon">
+            <FaUtensils />
+          </div>
+
           <h3>Fresh Food</h3>
-          <p>Enjoy fresh and delicious foods prepared with quality ingredients.</p>
+
+          <p>
+            Enjoy fresh and delicious foods prepared
+            with quality ingredients.
+          </p>
         </div>
 
-        <div className='featurecard'>
-          <div className='icon'>  <FaTicketAlt /></div>
+
+        <div className="featurecard">
+          <div className="icon">
+            <FaTicketAlt />
+          </div>
+
           <h3>Easy Coupons</h3>
-          <p>Purchase food coupons instantly with just a few clicks.</p>
+
+          <p>
+            Purchase food coupons instantly with
+            just a few clicks.
+          </p>
         </div>
 
-        <div className='featurecard'>
-          <div className='icon'>  <FaShippingFast /></div>
+
+        <div className="featurecard">
+          <div className="icon">
+            <FaShippingFast />
+          </div>
+
           <h3>Fast Service</h3>
-          <p>Get your food quickly without waiting in long queues.</p>
+
+          <p>
+            Get your food quickly without waiting
+            in long queues.
+          </p>
         </div>
 
-        <div className='featurecard'>
-          <div className='icon'> <FaShieldAlt /></div>
+
+        <div className="featurecard">
+          <div className="icon">
+            <FaShieldAlt />
+          </div>
+
           <h3>Secure Payment</h3>
-          <p>Safe and Secure payment methods for every tarnsaction.</p>
+
+          <p>
+            Safe and secure payment methods
+            for every transaction.
+          </p>
         </div>
 
       </div>
+
     </section>
-  )
+  );
 }
